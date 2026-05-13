438545fd9da816d1c99c82af9b0a5053af499c2e
ulianabracuk <s34972@pjwstk.edu.pl>
2026-05-13 22:39:07 +0200

    Dodano klase Main dla zadania 2

---
+++ src/zadanie2/Main.java
@@ -0,0 +1,16 @@
+package zadanie2;
+
+public class Main {
+    public static void main(String[] args) {
+        ServiceOrder order = new ServiceOrder("Firma Alfa", 10, 120.0);
+        PriceCalculator calculator = new PriceCalculator();
+
+        PriceStrategy standard = o -> o.hours() * o.hourRate();
+        PriceStrategy discount = o -> o.hours() * o.hourRate() * 0.90;
+        PriceStrategy weekend  = o -> o.hours() * o.hourRate() * 1.25;
+
+        System.out.println("Standard: " + calculator.calculate(order, standard));
+        System.out.println("Rabat:    " + calculator.calculate(order, discount));
+        System.out.println("Weekend:  " + calculator.calculate(order, weekend));
+    }
+}
